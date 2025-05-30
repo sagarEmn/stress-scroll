f6a1e7f82707ec8a0c76b1f1b40422a41e151019
Sagar <100994834+sagarEmn@users.noreply.github.com>
2025-05-30 22:31:32 +0545

    feat: Build basic Cycle structure

---
+++ src/components/Cycle.jsx
@@ -0,0 +1,13 @@
+const Cycle = () => {
+    return(
+        <section id="cycle" className="py-20 px-6 relative">
+            <div className="max-w-6xl mx-auto">
+                <div className="text-center mb-16">
+
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Cycle;
